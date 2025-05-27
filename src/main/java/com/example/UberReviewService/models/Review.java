541c8d401da61bf6c622c363cb4896b919c60318
Saeel Girish Dhatrak <saeel.dhatrak@Yash.com>
2025-05-27 19:53:32 +0530

    added BaseModel class using MappedSuperClass Annotation

---
+++ src/main/java/com/example/UberReviewService/models/Review.java
@@ -0,0 +1,26 @@
+package com.example.UberReviewService.models;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "bookingreview")
+public class Review extends BaseModel {
+
+    @Column(nullable = false)
+    private String content;
+
+    private Double rating;
+
+    @Override
+    public String toString(){
+        return "Review" + this.content + " " + this.rating + " " + this.createdAt;
+    }
+
+}
